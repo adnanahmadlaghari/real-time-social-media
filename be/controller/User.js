0908e65b40a72a5527335babf9542a69b10cd58b
Adnan Ahmad <laghariadnankhan7@gmail.com>
2025-06-11 15:37:05 +0500

    adding delete user function and its route

--- be/controller/User.js
+++ be/controller/User.js
@@ -79,10 +79,36 @@ const updateUser = async (req, res) => {
   }
 };
 
+const deleteUser = async (req, res) => {
+  try {
 
+    const {id} = req.params
+
+    const user = await User.findByIdAndDelete({_id: id})
+    
+    if(!user){
+      return res.status(404).json({
+        success: false,
+        error: "User Not Found"
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      msg: `User with ID: ${id} Deleted.`
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+      msg: "Internal Server Error",
+    });
+  }
+}
 
 module.exports = {
   getSingleUser,
   getAllUser,
   updateUser,
+  deleteUser
 };
--- be/routes/User.js
+++ be/routes/User.js
@@ -1,13 +1,13 @@
 
 
 const express = require("express")
-const { getSingleUser, getAllUser, updateUser } = require("../controller/User")
+const { getSingleUser, getAllUser, updateUser, deleteUser } = require("../controller/User")
 
 const userRoute = express.Router()
 
 userRoute.route("/").get(getAllUser)
 
 userRoute.route("/:username").get(getSingleUser)
-userRoute.route("/:id").patch(updateUser)
+userRoute.route("/:id").patch(updateUser).delete(deleteUser)
 
 module.exports = userRoute
