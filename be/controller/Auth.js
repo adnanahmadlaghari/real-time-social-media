3563f2a3910b516d4883e858836b741ae485e789
Adnan Ahmad <laghariadnankhan7@gmail.com>
2025-06-11 12:17:44 +0500

    install argon2 and use it for hashing in make passowrd hashed in register and check hashed password in login

--- be/controller/Auth.js
+++ be/controller/Auth.js
@@ -1,15 +1,18 @@
 const User = require("../models/User");
+const argon2 = require("argon2");
 
 const register = async (req, res) => {
   try {
     const { firstName, lastName, username, profile, password } = req.body;
+
+    const hashedPassword = await argon2.hash(password);
 
     const user = await User.create({
       firstName,
       lastName,
       username,
       profile,
-      password,
+      password: hashedPassword,
     });
 
     res.status(201).json({ success: true, user });
@@ -24,18 +27,26 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   try {
-
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    
-    if(!user){
-        return res.status(404).json({
-            success: false,
-            msg: "User Not Found"
-        })
-    }
-    res.status(200).json({ success: true, user });
 
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        error: "User Not Found",
+      });
+    }
+    const isPasswordValid = await argon2.verify(user.password, password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        success: false,
+        error: "Invalid Credential",
+      });
+    }
+    
+    res.status(200).json({ success: true, user });
   } catch (error) {
     res.status(500).json({
       success: false,
