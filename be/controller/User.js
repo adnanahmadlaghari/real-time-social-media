6bb839e88206a1f8ea8364251635ae247282913a
Adnan Ahmad <laghariadnankhan7@gmail.com>
2025-06-11 15:09:25 +0500

    adding update user functionality

--- be/controller/User.js
+++ be/controller/User.js
@@ -1,23 +1,74 @@
-const User = require("../models/User")
+const User = require("../models/User");
+const argon2 = require("argon2")
 
 
 const getSingleUser = async (req, res) => {
   try {
+    const { username } = req.params;
 
-    const {username} = req.params
+    const user = await User.findOne({ username }).select("-password");
 
-    const user = await User.findOne({username}).select("-password")
-
-    if(!user){
-        return res.status(404).json({
-            success: false,
-            error: "User Not Found"
-        })
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        error: "User Not Found",
+      });
     }
 
     res.status(200).json({
-        success: true,
-        user
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+      msg: "Internal Server Error",
+    });
+  }
+};
+
+const getAllUser = async (req, res) => {
+  try {
+    const users = await User.find({}).select("-password");
+    res.status(200).json({
+      success: true,
+      users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+      msg: "Internal Server Error",
+    });
+  }
+};
+
+const updateUser = async (req, res) => {
+
+  try {
+      const {id} = req.params
+      
+      const {firstName, lastName, username, password, profile} = req.body
+
+      const hashedPassword = await argon2.hash(password) 
+      const user = await User.findByIdAndUpdate({_id: id},{
+      firstName,
+      lastName,
+      username,
+      password: hashedPassword,
+      profile
+    }, {new: true, runValidators: true}).select('-password')
+
+    if(!user) {
+      return res.status(404).json({
+        success: false,
+        error: "User Not Found"
+      })
+    }
+    res.status(200).json({
+      success: true,
+      user
     })
   } catch (error) {
     res.status(500).json({
@@ -28,22 +79,10 @@ const getSingleUser = async (req, res) => {
   }
 };
 
-const getAllUser = async(req,res) => {
-    try {
-       const users = await User.find({}).select('-password')
-       res.status(200).json({
-        success: true,
-        users
-       }) 
-    } catch (error) {
-       res.status(500).json({
-      success: false,
-      error: error.message,
-      msg: "Internal Server Error",
-    }); 
-    }
-}
+
+
 module.exports = {
-    getSingleUser,
-    getAllUser,
-}
+  getSingleUser,
+  getAllUser,
+  updateUser,
+};
--- be/routes/User.js
+++ be/routes/User.js
@@ -1,12 +1,13 @@
 
 
 const express = require("express")
-const { getSingleUser, getAllUser } = require("../controller/User")
+const { getSingleUser, getAllUser, updateUser } = require("../controller/User")
 
 const userRoute = express.Router()
 
 userRoute.route("/").get(getAllUser)
 
 userRoute.route("/:username").get(getSingleUser)
+userRoute.route("/:id").patch(updateUser)
 
 module.exports = userRoute
