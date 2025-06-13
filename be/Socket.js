1bab307bd4a96820968a5b2313d83bbc014838fb
Adnan Ahmad <laghariadnankhan7@gmail.com>
2025-06-13 12:06:43 +0500

    create post using socket.io

--- be/Socket.js
+++ be/Socket.js
@@ -1,16 +1,34 @@
 const { Server } = require("socket.io");
+const Task = require("./models/Task");
 
 const setupSocket = (server) => {
   const io = new Server(server, {
     cors: {
       origin: "*", // change to your frontend URL
-      methods: ["GET", "POST"],
+      methods: ["GET", "POST", "PATCH", "DELETE"],
     },
   });
 
-  const userSocketMap = new Map();
+  const createPost = async(payload, callback) => {
+   try {
+   const {author, title, content} = payload
+
+  const post = await Task.create({
+      author,
+      title,
+      content
+    })
+    if(callback) callback({success: true, post})
+   } catch (error) {
+     if(callback) callback({success: false, error: error.message})
+   }
+
+  }
+
   io.on("connection", (socket) => {
     console.log("a user is connected");
+
+    socket.on("create-post", createPost)
 
     socket.on("disconnect", () => {
       console.log("user disconnected ", socket.id);
