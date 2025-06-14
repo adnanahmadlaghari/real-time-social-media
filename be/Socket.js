6b7037da43ed28e827ec8167d60c9edf8eaf20b3
Adnan Ahmad <laghariadnankhan7@gmail.com>
2025-06-14 14:04:33 +0500

    make crud of task using socket io make it realtime posts

--- be/Socket.js
+++ be/Socket.js
@@ -4,33 +4,73 @@ const Task = require("./models/Task");
 const setupSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "*", // change to your frontend URL
+      origin: "*",
       methods: ["GET", "POST", "PATCH", "DELETE"],
     },
   });
 
+  const createPost = async (payload, callback) => {
+    try {
+      const { author, title, content, media } = payload;
 
-  const createPost = async(payload, callback) => {
-   try {
-   const {author, title, content} = payload
+      const post = await Task.create({ author, title, content, media });
 
-  const post = await Task.create({
-      author,
-      title,
-      content
-    })
-    if(callback) callback({success: true, post})
-      console.log(post)
-   } catch (error) {
-     if(callback) callback({success: false, error: error.message})
-   }
+      io.emit("new-post", post); // broadcast to all
+      if (callback) callback({ success: true, post });
 
-  }
+      console.log("Created:", post);
+    } catch (error) {
+      if (callback) callback({ success: false, error: error.message });
+    }
+  };
+
+  const updatePost = async ({ id, data }, callback) => {
+    try {
+      const updatedPost = await Task.findByIdAndUpdate(id, data, { new: true });
+
+      if (!updatedPost) throw new Error("Post not found");
+
+      io.emit("updated-post", updatedPost);
+      if (callback) callback({ success: true, post: updatedPost });
+
+      console.log("Updated:", updatedPost);
+    } catch (error) {
+      if (callback) callback({ success: false, error: error.message });
+    }
+  };
+
+  const deletePost = async (id, callback) => {
+    try {
+      const deletedPost = await Task.findByIdAndDelete(id);
+
+      if (!deletedPost) throw new Error("Post not found");
+
+      io.emit("deleted-post", id);
+      if (callback) callback({ success: true, id });
+
+      console.log("Deleted:", deletedPost);
+    } catch (error) {
+      if (callback) callback({ success: false, error: error.message });
+    }
+  };
 
   io.on("connection", (socket) => {
-    console.log("a user is connected");
+    console.log("a user is connected", socket.id);
 
-    socket.on("create-post", createPost)
+    socket.on("get-all-posts", async (callback) => {
+      try {
+        const posts = await Task.find()
+          .populate("author", "firstName lastName username profile")
+          .sort({ createdAt: -1 });
+        callback({ success: true, posts });
+      } catch (err) {
+        callback({ success: false, error: err.message });
+      }
+    });
+
+    socket.on("create-post", createPost);
+    socket.on("update-post", updatePost);
+    socket.on("delete-post", deletePost);
 
     socket.on("disconnect", () => {
       console.log("user disconnected ", socket.id);
